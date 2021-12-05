b5e54eaf1f4164c097e7ca9002a6ff283e8a6977
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-05 14:55:04 +0530

    feat added
    In the script I have added/enabled the features

--- app.js
+++ app.js
@@ -2,18 +2,54 @@ const stopBtn = document.querySelector("#stop-btn");
 const speakBtn = document.querySelector("#speak-btn");
 const msg = new SpeechSynthesisUtterance();
 const voiceDropdown = document.querySelector("[name='voice']");
-const options = document.querySelectorAll("[type = 'range'],[name='text'] " );
+const options = document.querySelectorAll("[type = 'range'],[name='text'] ");
 
-let voices =[];
+let voices = [];
 
 msg.text = document.querySelector("[name='text']").value;
 
 
-function populateVoices(){
-    let voice =   this.getVoices();
+function populateVoices() {
+     voices = this.getVoices();
     // console.log(voice);
-    
+
+    const voiceOptions = voices
+        .map(v => `<option value ="${v.name}"> ${v.name} (${v.lang}) </option>`).join("")
+
+    // console.log(voiceOptions);
+    voiceDropdown.innerHTML = voiceOptions;
+
+
 }
 
-speechSynthesis.addEventListener("voiceschanged",populateVoices );
+
+function setVoice() {
+    msg.voice = voices.find(v => v.name === this.value  ); 
+    // console.log(msg.voice);
+    start_stop()
+
+}
+
+function start_stop (startOver = true) {
+    speechSynthesis.cancel();
+    if(startOver){
+    speechSynthesis.speak(msg);
+    }
+}
+
+function setOptions(){
+    console.log(this.name,this.value ) ;
+    msg[this.name] = this.value;
+    start_stop();
+}
+
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voiceDropdown.addEventListener("change",setVoice );
+options.forEach(items =>items.addEventListener("change",setOptions )  );
+speakBtn.addEventListener("click",start_stop  );
+stopBtn.addEventListener("click",() =>{
+    start_stop(false)
+}  )
+// console.log(voices);
 
